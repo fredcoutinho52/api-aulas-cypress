14cf42c9f2afa2b3f318dbe67eb7a443f05a97e1
Fred Coutinho <fredcoutinho52@gmail.com>
2022-04-06 11:49:36 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/formulario", (request, response) => {
+    const data = request.body;
+    
+    Object.entries(data).forEach(item => {
+        if (!item[1]) {
+            response.json({ message: "Formulário incompleto" });
+        }
+    });
+
+    response.json({ message: "Formulário enviado com sucesso" });
+});
+
+app.listen(3001, () => {
+    console.log("Servidor rodando");
+});
